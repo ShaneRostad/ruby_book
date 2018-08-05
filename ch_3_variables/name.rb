614e0d77ad4857a210b0be30d43fd119f25b8472
Shane Rostad <Shanegrostad@gmail.com>
2018-08-05 14:17:58 -0400

    These are my completed exercises for the variables section

---
+++ ch_3_variables/name.rb
@@ -0,0 +1,13 @@
+#Exercise 1 and 4
+puts "First Name: "
+first_name = gets.chomp
+puts "Last Name: "
+last_name = gets.chomp
+
+puts "Hey, " + first_name + " " + last_name + "!"
+
+#Exercise 3
+
+10.times do
+  puts first_name
+end
