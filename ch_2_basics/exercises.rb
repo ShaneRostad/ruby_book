b6bc269f7efef1f86f80afd4e32dd1edf7350d5e
Shane Rostad <Shanegrostad@gmail.com>
2018-08-05 13:33:38 -0400

    here are my exercises for the basics section

---
+++ ch_2_basics/exercises.rb
@@ -0,0 +1,37 @@
+#Exercise 1
+
+puts "Shane " + "Rostad"
+
+#Exercise 2
+
+thousands = puts 5423 / 1000
+hundreds = puts 5423 / 100  % 10
+tens = puts 5423 / 10 % 10
+ones = puts 5423 % 10
+
+#Exercise 3
+movie_titles = {lotr: "1975",
+                sandlot: "1999",
+                harry_potter: "2000" }
+
+
+puts movie_titles[:lotr]
+puts movie_titles[:sandlot]
+puts movie_titles[:harry_potter]
+
+#Exercise 4
+
+my_array = ["1975", "1999", "2000"]
+puts my_array
+
+#Exercise 5
+puts 5 * 4 * 3 * 2 * 1
+puts 6 * 5 * 4 * 3 * 2 * 1
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+#Exercise 6
+
+puts 3.54 ** 2
+puts 4.43 ** 2
+puts 3.46 ** 2
